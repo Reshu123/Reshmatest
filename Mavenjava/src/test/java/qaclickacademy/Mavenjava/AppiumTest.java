9fe494db1e7960e8e8bd1552df9ee6fc5d7899e5
Reshma <reshmaprabhu20@gmail.com>
2020-11-23 23:15:55 -0700

    first commit

---
+++ Mavenjava/src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+@Test
+	
+	public void AndroidAutomation()
+	{
+		System.out.println(" AndroidAutomation");
+	}
+	
+	@Test
+	public void IOSAutomation()
+	{
+		System.out.println("IOSAutomation");
+	}
+
+}
